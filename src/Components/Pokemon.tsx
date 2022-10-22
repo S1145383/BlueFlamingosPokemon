8784a4991243c9ccad8292cc50ba673957cdad2c
Jochem <s1145383@student.windesheim.nl>
2022-10-22 15:12:53 +0200

    FInished

--- src/Components/Pokemon.tsx
+++ src/Components/Pokemon.tsx
@@ -1,14 +1,14 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 
 interface iPokemon {
+    id: number,
     abilities: [],
     base_experience: number,
     forms: [],
     height: number,
+    weight: number,
     held_items: [],
-    id: number,
-    is_default: boolean,
     moves: [],
     name: string,
     order: number,
@@ -26,16 +26,23 @@ interface iPokemon {
         front_shiny: string,
         front_shiny_female: string
     },
-    stats: [],
+    stats: [
+        {
+            base_stat: number,
+            effort: number,
+            stat: {
+                name: string
+            }
+        }
+    ],
     types: []
 }
 
 function Pokemon() {
     const params = useParams();
-
     const pokemonId = params.pokemonId;
-
     const [currentPokemon, setCurrentPokemon] = useState<iPokemon>();
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`).then(result => {
@@ -44,6 +51,10 @@ function Pokemon() {
             })
         })
     }, [pokemonId])
+
+    let backButtonClicked = () => {
+        navigate("/")
+    }
 
     return (
         <div className="flex w-full">
@@ -57,30 +68,30 @@ function Pokemon() {
                         <img src={currentPokemon?.sprites.front_default} alt={""} width={64} height={64} >
                         </img>
                     </div>
-
-
 					<p className="text-sm font-semibold">
-						Wat een mooi ding!!
+						Wat een mooi ding!
 					</p>
-
 				</div>
 
 
+                <div className="mb2 font-bold">
+                    Statistieken:
+                </div>
 
-                {/*<div className="m-4">*/}
-                {/*    <button onClick={onPreviousButtonClicked}*/}
-                {/*            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-8">Previous*/}
-                {/*    </button>*/}
-                {/*    <button onClick={onNextButtonClicked}*/}
-                {/*            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-8">Next*/}
-                {/*    </button>*/}
-                {/*</div>*/}
-                {/*<p>*/}
-                {/*    Pagina {pagination / paginationAmount + 1}*/}
-                {/*</p>*/}
+                {currentPokemon.stats.map((obj, index) => {
+                    return <div className="flex my-2 justify-between" key={index}><p className="font-semibold mr-1 capitalize">{obj.stat.name}:</p>  <p>{obj.base_stat}</p></div>
+                })}
+
+                <div className="flex my-2 justify-between"><p className="font-semibold mr-1">Base experience:</p> <p>{currentPokemon.base_experience}</p></div>
+                <div className="flex my-2 justify-between"><p className="font-semibold mr-1">Height:</p> <p>{currentPokemon.height}</p></div>
+                <div className="flex my-2 justify-between"><p className="font-semibold mr-1">Weight:</p> <p>{currentPokemon.weight}</p></div>
+
+                <button onClick={backButtonClicked}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Terug
+                </button>
 
 			</div>
-                : <div>Wacht!</div>}
+                : <div>Aan het laden...</div>}
         </div>
     )
 
--- src/Components/PokemonList.tsx
+++ src/Components/PokemonList.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
-
 interface iPokemonListItem {
     name: string,
     url: string
@@ -9,28 +8,36 @@ interface iPokemonListItem {
 
 function PokemonList() {
 
-    let [pagination, setPagination] = useState<number>(0);
-    let paginationAmount = 15;
+    let [currentPagination, setCurrentPagination] = useState<number>(0);
+    let [totalItems, setTotalItems] = useState<number>(0);
+
+    let [paginationAmount, setPaginationAmount] = useState<number>(15)
+
+    let pagesAmount = Math.ceil(totalItems / paginationAmount);
+
     const navigate = useNavigate();
 
     let [pokemonList, setPokemonList] = useState<iPokemonListItem[]>([]);
 
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pagination}&limit=${paginationAmount}`).then(result => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?offset=${currentPagination}&limit=${paginationAmount}`).then(result => {
                 result.json().then(obj => {
                     setPokemonList(obj.results)
+                    setTotalItems(obj.count)
                 })
             }
         )
-    }, [pagination, paginationAmount])
+    }, [currentPagination, paginationAmount])
 
     let onNextButtonClicked = () => {
-        setPagination(pagination + paginationAmount);
+        if(currentPagination + paginationAmount <= totalItems){
+            setCurrentPagination(currentPagination + paginationAmount);
+        }
     }
 
     let onPreviousButtonClicked = () => {
-        if (pagination - paginationAmount >= 0) {
-            setPagination(pagination - paginationAmount);
+        if (currentPagination - paginationAmount >= 0) {
+            setCurrentPagination(currentPagination - paginationAmount);
         }
     }
 
@@ -38,9 +45,13 @@ function PokemonList() {
         navigate("/pokemon/" + (id + 1) )
     }
 
+    let onPaginationSelected = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setPaginationAmount(parseInt(event.target.value));
+    }
+
     return (
-        <div className="flex w-full">
-            <div className="m-auto mt-8 p-8 shadow-md bg-gray-100">
+        <div className="flex w-screen">
+            <div className="m-auto mt-8 lg:p-8 shadow-md bg-gray-100">
                 <div className="mb-2">
                     <h1 className="font-medium leading-tight text-5xl mt-0 text-blue-600">
                         Pokemonnen
@@ -51,24 +62,39 @@ function PokemonList() {
                 </div>
                 <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
                     {pokemonList.map((item, index) => {
-                        return <li key={index} onClick={() => { onPokemonRecordClicked(index + pagination)}}
+                        return <li key={index} onClick={() => { onPokemonRecordClicked(index + currentPagination)}}
                                    className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg hover:bg-blue-700 hover:text-white capitalize">{item.name}
                         </li>;
                     })}
                 </ul>
 
 
-                <div className="m-4">
+                <div className="m-4 flex justify-between">
                     <button onClick={onPreviousButtonClicked}
-                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-8">Previous
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 w-24 rounded">Vorige
                     </button>
                     <button onClick={onNextButtonClicked}
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-8">Next
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-24 rounded">Volgende
                     </button>
                 </div>
-                <p>
-                    Pagina {pagination / paginationAmount + 1}
-                </p>
+                <div className="flex justify-between">
+                    <p>
+                        Pagina {currentPagination / paginationAmount + 1} van {pagesAmount}
+                    </p>
+                    <div className="flex">
+                        <p>
+                            Aantal per pagina:
+                        </p>
+                        <select onChange={onPaginationSelected} defaultValue={15}>
+                            <option value={10}>10</option>
+                            <option value={15}>15</option>
+                            <option value={20}>20</option>
+                            <option value={40}>40</option>
+                        </select>
+                    </div>
+
+                </div>
+
 
             </div>
         </div>
